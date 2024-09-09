ee4776cbb57fba1d06f1c00857228dfc4d932afc
SamHos04 <samuelhostetter@gmail.com>
2024-09-09 18:38:09 -0500

    Changes to Experiment 1

--- Experiments/Hostetter_Sam/exp1/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiments/Hostetter_Sam/exp1/helloworld/src/main/java/coms309/WelcomeController.java
@@ -12,8 +12,29 @@ class WelcomeController {
         return "Hello and welcome to COMS 309";
     }
 
+    /*
+    This allows you to after localhost:8080/"EnterNameHere" for your name to show at the end.
+     */
     @GetMapping("/{name}")
     public String welcome(@PathVariable String name) {
         return "Hello and welcome to COMS 309: " + name;
     }
+
+    @GetMapping("/welcome/{name}/{age}")
+    public String welcomeWithAge(@PathVariable String name, @PathVariable int age) {
+        return "Hello and welcome to COMS 309, " + name + ". You are " + age + " years old.";
+    }
+    @GetMapping("/user/{id}")
+    public String welcomeUser(@PathVariable int id){
+        if (id==1) {
+            return "Hello and welcome to COMS 309, John.";
+        }
+        if (id==2) {
+            return "Hello and welcome to COMS 309, Jane";
+        }
+        else{
+            return "Error no user";
+        }
+    }
+
 }
