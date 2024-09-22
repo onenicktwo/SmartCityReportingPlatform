762a5b78ea818f847e4c4c7a049b5c0a48d1b39d
TheSausageSwiper <97609792+onenicktwo@users.noreply.github.com>
2024-09-21 20:21:24 -0500

    Removed useless imports

--- Backend/CityWatcherBackend/src/main/java/org/citywatcher/config/SecurityConfig.java
+++ Backend/CityWatcherBackend/src/main/java/org/citywatcher/config/SecurityConfig.java
@@ -2,8 +2,6 @@ package org.citywatcher.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
--- Backend/CityWatcherBackend/src/main/java/org/citywatcher/controller/UserController.java
+++ Backend/CityWatcherBackend/src/main/java/org/citywatcher/controller/UserController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/citywatcher/users")
