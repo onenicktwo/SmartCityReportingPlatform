beaf1b79da53fe204202ee4308be7f4e067a40a8
Sam Rowland <srowland@iastate.edu>
2024-11-17 23:51:25 -0600

    Testing CI/CD

--- Frontend/CityWatcherFrontend/app/src/main/java/com/example/citywatcherfrontend/CityWatcherActivity.java
+++ Frontend/CityWatcherFrontend/app/src/main/java/com/example/citywatcherfrontend/CityWatcherActivity.java
@@ -15,8 +15,6 @@ import org.java_websocket.handshake.ServerHandshake;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-//
-
 public abstract class CityWatcherActivity extends AppCompatActivity implements WebSocketListener {
 
     protected final String MAPS_KEY = "AIzaSyBWM0V9EhmOR0jCz7NYo9xDcmOW6Ni-trc";
