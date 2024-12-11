934049b2a060deabc94817317e5c57810b0e173e
onenicktwo <nmorrow619@gmail.com>
2024-12-11 14:47:10 -0600

    new pipeline test

--- Backend/CityWatcherBackend/src/main/java/org/citywatcher/CityWatcherApplication.java
+++ Backend/CityWatcherBackend/src/main/java/org/citywatcher/CityWatcherApplication.java
@@ -3,7 +3,6 @@ package org.citywatcher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Testing runner
 @SpringBootApplication
 public class CityWatcherApplication {
     public static void main(String[] args) {
